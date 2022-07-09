8fcb5a50358978d13e747e26c5de87f537318f18
Niten Solanki <94343010+niten-solanki@users.noreply.github.com>
2022-07-10 02:21:07 +0530

    address crud apis

---
+++ validations/address.js
@@ -0,0 +1,24 @@
+const Joi = require('@hapi/joi')
+
+class AddressValicationSchema {
+    static create = Joi.object({
+        name: Joi.string().required(),
+        phone: Joi.string().required().length(10)
+    });
+
+    static delete = Joi.object({
+        id: Joi.string().required()
+    })
+
+    static update = Joi.object({
+        id: Joi.string().required("Id is required!"),
+        name: Joi.string(),
+        phone: Joi.string().length(10)
+    })
+
+}
+
+
+module.exports = {
+    AddressValicationSchema
+}
